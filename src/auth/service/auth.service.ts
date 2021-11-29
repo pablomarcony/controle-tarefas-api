3798ea17700360e0d14c6e08953c20b5afebf581
lucas.ramos <lucas.ramos@seati.ma.gov.br>
2021-11-29 09:53:36 -0300

    Correção de login

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
     async login(user: any) {
         const payload = { nome: user.nome, sub: user.id };
         return {
-            access_token: this.jwtService.signAsync(payload),
+            access_token: this.jwtService.sign(payload),
         };
     }
 
