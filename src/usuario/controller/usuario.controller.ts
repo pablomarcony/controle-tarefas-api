250856df4035ebe1abb38945798dceae6872ca47
lucas.ramos <lucas.ramos@seati.ma.gov.br>
2021-12-21 17:38:39 -0300

    Rotas protegidas.

--- src/usuario/controller/usuario.controller.ts
+++ src/usuario/controller/usuario.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, UseGuards  } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, UseGuards, Req  } from '@nestjs/common';
 import { UsuarioService } from '../service/usuario.service';
 import { CreateUsuarioDto } from '../dto/create-usuario.dto';
 import { UpdateUsuarioDto } from '../dto/update-usuario.dto';
 import { AuthService } from 'src/auth/service/auth.service';
 import { LocalAuthGuard } from 'src/auth/guard/local-auth.guard';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UsuarioLoginDto } from '../dto/usuario-login.dto';
 
 @ApiTags('usuario')
@@ -16,6 +16,8 @@ export class UsuarioController {
     private authService: AuthService
   ) {}
 
+  @ApiBearerAuth('access_token')
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuarioService.create(createUsuarioDto);
@@ -28,16 +30,22 @@ export class UsuarioController {
     return await this.authService.login(usuario);  
   }
 
+  @ApiBearerAuth('access_token')
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.usuarioService.findAll();
   }
 
+  @ApiBearerAuth('access_token')
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.usuarioService.findOne(+id);
   }
 
+  @ApiBearerAuth('access_token')
+  @UseGuards(JwtAuthGuard)
   @Patch('update/:id')
   async update(@Param('id') id: number, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     const usuario = await this.usuarioService.findOne(id);
@@ -47,11 +55,26 @@ export class UsuarioController {
     return this.usuarioService.update(updateUsuarioDto);
   }
 
+  @ApiBearerAuth('access_token')
+  @UseGuards(JwtAuthGuard)
+  @Patch('perfil')
+  async updatePerfil(@Req() req, @Body() updateUsuarioDto: UpdateUsuarioDto) {
+    const usuario = await this.usuarioService.findOne(req.user.userId);
+    if(!usuario){
+      throw new BadRequestException("Usuário não encontrado.")
+    }
+    return this.usuarioService.update(updateUsuarioDto);
+  }
+
+  @ApiBearerAuth('access_token')
+  @UseGuards(JwtAuthGuard)
   @Patch('password')
   updatePassword(@Body() updateUsuarioDto: UpdateUsuarioDto){
     return this.usuarioService.update(updateUsuarioDto);
   }
 
+  @ApiBearerAuth('access_token')
+  @UseGuards(JwtAuthGuard)
   @Delete('delete/:id')
   remove(@Param('id') id: number) {
     return this.usuarioService.remove(+id);
